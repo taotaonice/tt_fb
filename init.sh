6315897c82f7fbbb0faea1fdcbbcfe65b9c2ef5f
taotaonice <hutaonice@gmail.com>
2021-04-05 12:51:16 +0800

    add init.sh

---
+++ init.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+sudo apt install xserver-xorg-core tigervnc-* ubuntu-gnome-desktop 
+
+mkdir ~/.vnc/
+
+echo '[ -x /etc/vnc/xstartup ] && exec /etc/vnc/xstartup' >> ~/.vnc/xstartup
+echo '[ -r $HOME/.Xresources ] && xrdb $HOME/.Xresources' >> ~/.vnc/xstartup
+echo 'vncconfig -iconic &' >> ~/.vnc/xstartup
+echo 'dbus-launch --exit-with-session gnome-session &' >> ~/.vnc/xstartup
+
+echo 'vncserver -localhost no -geometry 800x600 -depth 24 :1' > ~/vnc.sh
+echo 'vncserver -kill :1' > ~/kill.sh
+chmod +x ~/*.sh
